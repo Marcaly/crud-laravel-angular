6455deb5ee8007e8f5407add83ac804466623021
Gabryel Marçal <byelmarcal@hotmail.com>
2023-11-29 15:05:41 -0300

    arrumei Cors do php

---
+++ backend/app/Http/Middleware/Cors.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class Cors
+{
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+        $response->header('Access-Control-Allow-Origin', 'http://localhost:4200');
+        $response->header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+        $response->header('Access-Control-Allow-Headers', 'Content-Type');
+
+        return $response;
+    }
+}
