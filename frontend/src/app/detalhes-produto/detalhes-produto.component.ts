256cc6bd5b8438e7c6e0ab0d2e30a2b7a80d8945
Gabryel Marçal <byelmarcal@hotmail.com>
2023-11-29 15:07:12 -0300

    Implementei metodo detalhes do produto

---
+++ frontend/src/app/detalhes-produto/detalhes-produto.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { Produto } from '../models/produto';
+import { ActivatedRoute } from '@angular/router';
+import { ProdutoService } from '../services/produto.service';
+
+@Component({
+  selector: 'app-detalhes-produto',
+  templateUrl: './detalhes-produto.component.html',
+  styleUrl: './detalhes-produto.component.css'
+})
+export class DetalhesProdutoComponent {
+
+  id!: number;
+  produto!: Produto;
+  constructor(
+    private route: ActivatedRoute,
+    private produtoService: ProdutoService
+  ) {}
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+
+    this.produto = new Produto();
+    this.produtoService.mostrarProdutosPorId(this.id).subscribe((data) => {
+      this.produto = data;
+    });
+  }
+}
