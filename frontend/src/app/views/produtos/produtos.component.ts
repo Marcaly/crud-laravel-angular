d755dfd7d76b6485c0ff48de125728f33738b054
Gabryel Marçal <byelmarcal@hotmail.com>
2023-11-29 10:44:08 -0300

    implementei produtos.component.ts

--- frontend/src/app/views/produtos/produtos.component.ts
+++ frontend/src/app/views/produtos/produtos.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Produto from '../../models/Produto';
 
 @Component({
   selector: 'app-produtos',
   templateUrl: './produtos.component.html',
   styleUrl: './produtos.component.css'
 })
-export class ProdutosComponent {
+export class ProdutosComponent implements OnInit{
+
+  produtos:Produto[] = [{
+    id:1,
+    nome_produto:'Batata',
+    categoria_id: 2,
+    valor_produto: 5.7,
+    data_vencimento: new Date(14/12/2000),
+    quantidade_estoque: 2,
+    produto_perecivel: true
+    
+
+  }]
+  displayedColumns: string[] = ['nome_produto', 'categoria_id', 'valor_produto', 'data_vencimento', 'quantidade_estoque', 'produto_perecivel', 'acoes' ]
+
+  constructor() {}
+
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  atualizarProduto(produto: Produto) {
+
+  }
+
+  deletarProduto(id:number){}
 
 }
