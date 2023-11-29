ec9d08ab9a6d58e57e66cf634411bccfde43bb42
Gabryel Marçal <byelmarcal@hotmail.com>
2023-11-29 15:06:43 -0300

    implementei Método atualizar produto

---
+++ frontend/src/app/atualizar-produto/atualizar-produto.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { Produto } from '../models/produto';
+import { ProdutoService } from '../services/produto.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-atualizar-produto',
+  templateUrl: './atualizar-produto.component.html',
+  styleUrl: './atualizar-produto.component.css'
+})
+export class AtualizarProdutoComponent implements OnInit {
+
+  id!: number;
+  produto: Produto = new Produto();
+
+  constructor(
+    private produtoService: ProdutoService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+
+  ngOnInit(): void {
+  this.id = this.route.snapshot.params['id'];
+  
+  this.produtoService.mostrarProdutosPorId(this.id).subscribe(
+    data => {
+      this.produto = data;
+    },
+    error => console.log(error)
+  );
+  }
+
+  onSubmit() {
+    this.produtoService.atualizarProduto(this.produto,this.id).subscribe(
+      data => {
+        this.listaProdutos();
+      },
+      error => console.log(error)
+      );
+  }
+  listaProdutos() {
+    this.router.navigate(['/produtos'])
+  }
+}
